8d78a006f8eb5d2896f7028a1419be4bb194a253
Reis Da Silva <william.silva2@yrbrands.com>
2018-11-18 16:58:35 -0200

    footer

--- footer.php
+++ footer.php
@@ -1,121 +1,64 @@
 <footer class="main-footer dark-section">
-		<div class="row row-widgets">
-			<div class="columns small-12 medium-6 large-3">
-				<section class="widget widget-address">
-					<h3 class="widget-title">Contacts</h3>
-					<p>The 2nd Building, 27 Seventh Avenue, New York, 10787, United States</p>
-					<p class="widget-address__info icon-phone-3">+44 12 3456 7788</p>
-					<p class="widget-address__info icon-mail-3"><a href="mailto:lawyerburo@law.com">lawyerburo@law.com</a></p>
-				</section>
-				<section class="widget widget-subscribe">
-					<h3 class="widget-title">KEEP IN TOUCH WITH US</h3>
-					<p>Information about current events related to our company</p>
-					<form>
-						<input type="text" placeholder="Enter your e-mail">
-						<button type="submit" class="widget-subscribe__submit icon-paper-plane"></button>
-					</form>
-				</section>
-
-			</div>
-			<div class="columns small-12 medium-6 large-3">
-				<section class="widget widget_pages">
-					<h3 class="widget-title">EXPLORE OUR SITE</h3>
-					<ul>
-						<li><a href="people.html">People</a></li>
-						<li><a href="about.html">About Us</a></li>
-						<li><a href="location.html">Locations</a></li>
-						<li><a href="careers.html">Careers</a></li>
-						<li><a href="#">Publications</a></li>
-						<li><a href="news.html">News</a></li>
-						<li><a href="contact.html">Contact Us</a></li>
-					</ul>
-				</section>
-			</div>
-			<div class="columns small-12 medium-6 large-3">
-				<section class="widget widget_categories">
-					<h3 class="widget-title">PRACTICE AREAS</h3>
-					<ul>
-						<li class="cat-item">
-							<a href="single-practice.html">Banking & Finance</a>
-						</li>
-						<li class="cat-item">
-							<a href="single-practice.html">Corporate</a>
-						</li>
-						<li class="cat-item">
-							<a href="single-practice.html">Civil Litigation</a>
-						</li>
-						<li class="cat-item">
-							<a href="single-practice.html">Data Protection & Privacy</a>
-						</li>
-						<li class="cat-item">
-							<a href="single-practice.html">Insurance</a>
-						</li>
-						<li class="cat-item">
-							<a href="single-practice.html">Real estate</a>
-						</li>
-						<li class="cat-item">
-							<a href="single-practice.html">Tax, Employment, Pensions</a>
-						</li>
-						<li class="cat-item">
-							<a href="single-practice.html">Environment</a>
-						</li>
-					</ul>
-				</section>
-			</div>
-			<div class="columns small-12 medium-6 large-3">
-				<section class="widget widget-latest-posts">
-					<h3 class="widget-title">RECENT POSTS</h3>
-					<ul>
-						<li>
-							<a class="widget__date" href="news.html">March 18, 2017</a>
-							<h4><a href="single_news.html">America's Best Corporate Law Firms</a></h4>
-						</li>
-						<li>
-							<a class="widget__date" href="news.html">March 18, 2017</a>
-							<h4><a href="single_news.html">Managing tax risks in challenging times</a></h4>
-						</li>
-						<li>
-							<a class="widget__date" href="news.html">March 18, 2017</a>
-							<h4><a href="single_news.html">Top consumer law issues we were talking about in 2015</a></h4>
-						</li>
-					</ul>
-				</section>
-			</div>
+	<div class="row row-widgets">
+		<div class="columns small-12 medium-6 large-3">
+			<section class="widget widget-address">
+				<h3 class="widget-title">Endereço</h3>
+				<p>The 2nd Building, 27 Seventh Avenue, New York, 10787, United States</p>
+				<p class="widget-address__info icon-phone-3">+44 12 3456 7788</p>
+				<p class="widget-address__info icon-mail-3"><a href="mailto:lawyerburo@law.com">lawyerburo@law.com</a></p>
+			</section>
 		</div>
-		<div class="row">
-			<div class="columns small-12">
-				<div class="row align-justify align-middle logo-socials-footer small-collapse">
-					<div class="columns small-12 medium-expand">
-						<div class="logo">
-							<a href="index.html">
-								<img src="images/logo-footer.png" alt="Logo">
-							</a>
-						</div>
+		<div class="columns small-12 medium-6 large-3">
+			<section class="widget widget_pages">
+				<h3 class="widget-title">Mapa do Site</h3>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="sobre-nos.php">Sobre Nós</a></li>
+					<li><a href="servicos.php">Serviços</a></li>
+					<li><a href="contato.php">Conato</a></li>
+				</ul>
+			</section>
+		</div>
+		<div class="columns small-12 medium-6 large-6">
+			<section class="widget widget_categories">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.2673119919586!2d-46.66105438559654!3d-23.558841067383025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59cd0868cc7b%3A0xb5cb3665652f55ef!2sAv.+Paulista%2C+2000+-+Consola%C3%A7%C3%A3o%2C+S%C3%A3o+Paulo+-+SP%2C+01310-200!5e0!3m2!1spt-BR!2sbr!4v1542560990243" width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</section>
+		</div>
+	</div>
+	<div class="row">
+		<div class="columns small-12">
+			<div class="row align-justify align-middle logo-socials-footer small-collapse">
+				<div class="columns small-12 medium-expand">
+					<div class="logo">
+						<a href="index.html">
+							<img src="images/logo-footer.png" alt="Logo">
+						</a>
 					</div>
-					<div class="columns small-12 shrink">
-						<div class="socials">
-							<a href="#"><i class="icon-twitter"></i></a>
-							<a href="#"><i class="icon-facebook"></i></a>
-							<a href="#"><i class="icon-rss-1"></i></a>
-							<a href="#"><i class="icon-linkedin"></i></a>
-							<a href="#"><i class="icon-pinterest-4"></i></a>
-							<a href="#"><i class="icon-gplus-1"></i></a>
-							<a href="#"><i class="icon-instagram"></i></a>
-						</div>
+				</div>
+				<div class="columns small-12 shrink">
+					<div class="socials">
+						<a href="#"><i class="icon-twitter"></i></a>
+						<a href="#"><i class="icon-facebook"></i></a>
+						<a href="#"><i class="icon-rss-1"></i></a>
+						<a href="#"><i class="icon-linkedin"></i></a>
+						<a href="#"><i class="icon-pinterest-4"></i></a>
+						<a href="#"><i class="icon-gplus-1"></i></a>
+						<a href="#"><i class="icon-instagram"></i></a>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="row">
-			<div class="columns small-12">
-				<div class="row align-justify align-middle copyright small-collapse">
-					<div class="columns small-12 medium-expand">
-						Copyright © 2018. ThemeMakers. All rights reserved
-					</div>
-					<div class="columns small-12 shrink">
-						Developed by <a href="https://themeforest.net/user/thememakers/portfolio">ThemeMakers</a>
-					</div>
+	</div>
+	<div class="row">
+		<div class="columns small-12">
+			<div class="row align-justify align-middle copyright small-collapse">
+				<div class="columns small-12 medium-expand">
+					Copyright © 2018. ThemeMakers. All rights reserved
+				</div>
+				<div class="columns small-12 shrink">
+					Developed by <a href="https://themeforest.net/user/thememakers/portfolio">ThemeMakers</a>
 				</div>
 			</div>
 		</div>
-	</footer>
+	</div>
+</footer>
--- head.php
+++ head.php
@@ -13,42 +13,20 @@
 	<link rel="stylesheet" href="css/foundation/foundation.css">
 	<link rel="stylesheet" href="css/swiper.min.css">
 	<link rel="stylesheet" href="css/magnific-popup.css">
+	
 	<!-- custom -->
 	<link rel="stylesheet" href="css/main.css">
 	<!-- LayerSlider stylesheet -->
 	<link rel="stylesheet" href="layerslider/css/layerslider.css" type="text/css">
-	<!-- google map location -->
-	<script>
-		var geoAddress = [
-			{
-				address: 'New York, NY, USA',
-				defaultMarker: false,
-				style: true,
-				zoom: 12
-			},
-			{
-				address: 'Amsterdam, Netherlands'
-			},
-			{
-				address: 'Bangkok, Thailand'
-			},
-			{
-				address: 'Abu Dhabi'
-			},
-			{
-				address: 'Washington'
-			},
-			{
-				address: 'Moscow'
-			}
-		];
-	</script>
+	<!-- CSS MODIFI -->
+	
 	<link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
 	<link rel="icon" type="image/png" href="images/favicon/favicon-32x32.png" sizes="32x32">
 	<link rel="icon" type="image/png" href="images/favicon/favicon-16x16.png" sizes="16x16">
 	<link rel="manifest" href="images/favicon/manifest.json">
 	<link rel="mask-icon" href="images/favicon/safari-pinned-tab.svg">
 	<link rel="shortcut icon" href="images/favicon/favicon.ico">
+	<link rel="stylesheet" href="css/default.css">
 	<meta name="msapplication-TileColor" content="#ffc40d">
 	<meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png">
 	<meta name="msapplication-config" content="images/favicon/browserconfig.xml">
